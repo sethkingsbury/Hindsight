de396aa600b272e6eb2aef843d7f96433a9cdc50
Seth Kingsbury <sethkingsbury@gmail.com>
2022-07-28 00:13:15 +1200

    formatted home, login and register pages. Including setting reuqired fields and basic validation for logging in and registering.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
+import { FaArrowRight, FaRegWindowMaximize } from 'react-icons/fa';
+
 function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<>
+			<section className='heading'>
+				<h1>Welcome</h1>
+				<p>Choose an option below to get started!</p>
+			</section>
+
+			<Link to='/createRetro' className='btn btn-reverse btn-block'>
+				<FaRegWindowMaximize /> Create a Retro
+			</Link>
+
+			<Link to='/joinRetro' className='btn btn-block'>
+				<FaArrowRight /> Join a Retro
+			</Link>
+		</>
 	);
 }
 
